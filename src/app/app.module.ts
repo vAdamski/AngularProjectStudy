6f6d0f83030745e41a78573590f5d3fa3cbc40c7
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2021-12-20 02:01:17 +0100

    Big update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,18 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     RegisterComponent,
     LoginComponent,
     TicketComponent,
-    TopBarComponent
+    TopBarComponent,
+    HomeComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'register', component: RegisterComponent },
+      {path: 'login', component: LoginComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
